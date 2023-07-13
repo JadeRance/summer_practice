b8844f52312b1ebde8e5aae561050d0a48c9e6ed
Q100V <dvk97@tpu.ru>
2023-07-13 12:04:28 +0700

    Pt_1_5: Football scores
    Determine the outcome of the game by the score earned by the team

---
+++ Pt_1_5.py
@@ -0,0 +1,12 @@
+print("Введите баллы команды: ")
+points = int(input())
+possible_case = [0, 1, 3]
+while(not(points in possible_case)):
+    print("Указано неверное число, повторите попытку: ")
+    points = int(input())
+if points == 0:
+    print("Команда проиграла.")
+if points == 1:
+    print("Команда сыграла вничью.")
+if points == 3:
+    print("Команда выиграла.")
