fa7adf3ae03654f1500fb44de0bee63cc1c7ee3a
Q100V <dvk97@tpu.ru>
2023-07-13 12:24:00 +0700

    Pt_1_9: Multiplication
    Enter two numbers to find their multiplication.

---
+++ Pt_1_9.py
@@ -0,0 +1,4 @@
+print("Введите два числа для нахождения их произведения: ")
+first = int(input())
+second = int(input())
+print("Произведение чисел ", first, " и ", second, " равно ", first*second, sep='')
