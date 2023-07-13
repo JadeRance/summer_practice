aa32ff31482c4c04ce3b2988337a64f4c3e50bc0
Q100V <dvk97@tpu.ru>
2023-07-13 11:50:54 +0700

    Pt_1_4: Enter number between 10 and 20
    Enter the number. If the number is lower than 10 or greater than 20,
    enter the number again.

---
+++ Pt_1_4.py
@@ -0,0 +1,7 @@
+print("Введите число от 10 до 20: ")
+InputNumber = int(input())
+while(InputNumber < 10 or InputNumber > 20):
+    if InputNumber < 10: print("Число меньше требуемого диапозона. Повторите попытку: ")
+    if InputNumber > 20: print("Число больше требуемого диапозона. Повторите попытку: ")
+    InputNumber = int(input())
+print("Спасибо")
