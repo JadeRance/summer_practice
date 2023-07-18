42d608502ede9d015ca911e4a1e50f99859ea89f
Q100V <dvk97@tpu.ru>
2023-07-18 12:31:00 +0700

    Pt_2_2: Television programm
    There is a list with a television program. Add new broadcast to this
    list without breaking the order of included broadcasts.

---
+++ Pt_2_2.py
@@ -0,0 +1,10 @@
+tv_list = ["Пусть говорят", "Ревизорро", "Орёл и решка", "Кухня"]
+for i in range(len(tv_list)):
+    print('#', i+1, ' ', tv_list[i], sep='')
+print("Введите название программы: ")
+telecast = input()
+print("Введите номер для программы в списке: ")
+pos = int(input())
+tv_list.insert(pos-1, telecast)
+for i in range(len(tv_list)):
+    print('#', i+1, ' ', tv_list[i], sep='')
