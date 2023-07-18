28fa1e95fa3a70c3ecdd874e7e6abe2de4fba9df
Q100V <dvk97@tpu.ru>
2023-07-18 12:47:48 +0700

    Pt_2_5: Toss game
    Try to guess side of the coin. After the end of the game you will
    have your score: number of correct guesses and wrong guesses.

---
+++ Pt_2_5.py
@@ -0,0 +1,18 @@
+import random
+win_count = lose_count = lose = 0
+while (lose < 3):
+    turn = random.randint(0, 1)
+    print("Орёл или решка?")
+    player = int(input())
+    if player != 0 and player != 1:
+        break
+    if player == turn:
+        print("Верно!")
+        win_count += 1
+        lose = 0
+    else:
+        print("Неверно!")
+        lose_count += 1
+        lose += 1
+print("Конец игры")
+print("Количество побед: ", win_count, '\n', "Количество поражений: ", lose_count, sep='')
