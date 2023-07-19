e493b43f85d975b21c3cc0a981c05b477c354fef
Q100V <dvk97@tpu.ru>
2023-07-20 01:34:34 +0700

    Pt_3_7: Vowel or consonant
    Enter the string to determine defenition of each letter in the
    string: is it vowel or consonant.

--- Pt_3_7.py
+++ Pt_3_7.py
@@ -1,6 +1,6 @@
 x = input()
 x_list = list(x.lower())
 print(x_list)
-check = lambda x: "Гласная" if x in "ауоыиэяюёе" else "Согласная"
+check = lambda x: "True" if x in "ауоыиэяюёе" else "False"
 dict = {x_list[i]: check(x_list[i]) for i in range(len(x_list))}
 print(dict)
