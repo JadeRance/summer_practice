742619903e02a204e790781e28a2968c1a8da107
Q100V <dvk97@tpu.ru>
2023-07-22 11:51:38 +0700

    Pt_5_1: Book list
    Creates .csv file with book list.

--- Pt_5_1.py
+++ Pt_5_1.py
@@ -5,7 +5,7 @@ data = [['', 'Книга', 'Автор', 'Год выпуска'],
         ['1', 'World of Warcraft: Иллидан', 'Уильям Кинг', '2016'],
         ['2', 'Преступление и наказание', 'Фёдор Достоевский', '1866'],
         ['3', 'Приключения Тома Сойера', 'Марк Твен', '1876'],
-        ['4', 'Dragon Age: The World of Thedas Volume 2', 'Дэвид Гейдер', '2015']]
+        ['4', 'Dragon Age: The World of Thedas 2', 'Дэвид Гейдер', '2015']]
 
 with open('books.csv', 'w+', encoding='cp1251', newline='') as f:
     writer = csv.writer(f)
