5e04ee6fdabcf128e7e651e79a9228ca79ddccf5
Q100V <dvk97@tpu.ru>
2023-07-22 11:47:14 +0700

    Pt_4_6: Algorithm ROT13
    Enter the string to change indeces of letters.

--- Pt_4_6.py
+++ Pt_4_6.py
@@ -2,14 +2,14 @@ enter = input()
 letters = list(enter.lower())
 for i in range(len(letters)):
     if letters[i] != ' ':
-        if ord(letters[i]) > 96 and  ord(letters[i]) < 123:
-            if ord(letters[i])+2 > 122:
-                letters[i] = chr(ord(letters[i])+2-26)
+        if ord(letters[i]) > 96 and ord(letters[i]) < 123:
+            if ord(letters[i]) + 2  > 122:
+                letters[i] = chr(ord(letters[i]) + 2 - 26)
             else:
-                letters[i] = chr(ord(letters[i])+2)
-        if ord(letters[i]) > 1071 and  ord(letters[i]) < 1104:
-            if ord(letters[i])+2 > 1104:
-                letters[i] = chr(ord(letters[i])+2-32)
+                letters[i] = chr(ord(letters[i]) + 2)
+        if ord(letters[i]) > 1071 and ord(letters[i]) < 1104:
+            if ord(letters[i]) + 2 > 1104:
+                letters[i] = chr(ord(letters[i]) + 2 - 32)
             else:
-                letters[i] = chr(ord(letters[i])+2)
+                letters[i] = chr(ord(letters[i]) + 2)
 print("".join(letters))
