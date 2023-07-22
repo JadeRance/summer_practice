e37c4ccf289e95678c4d163ff2333ad4f2d84c6b
Q100V <dvk97@tpu.ru>
2023-07-22 10:03:44 +0700

    Pt_1_3: Sorting numbers
    Enter three numbers to find the greatest and sort them in descending
    order.

--- Pt_1_3.py
+++ Pt_1_3.py
@@ -1,5 +1,6 @@
 first = int(input())
 second = int(input())
 third = int(input())
-print("Наибольшее число - ", max(first, second, third))
-print(max(first, second, third), first+second+third-max(first, second, third)-min(first, second, third), min(first, second, third))
+print("Наибольшее число -", max(first, second, third))
+print(max(first, second, third), first + second + third - max(first,
+    second, third) - min(first, second, third), min(first, second, third))
