2ee332a73bc8da040e84ebf169c842539e9c6384
Q100V <dvk97@tpu.ru>
2023-07-22 11:16:16 +0700

    Pt_3_2: Fibonacci numbers
    Return list of fibonacci numbers up to entered value.

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,13 +1,17 @@
 import math
 from functools import reduce
 
+
 def find_quantity(n):
     x = [0, 1]
     while (x[-1] <= n):
-        x.append(x[-1]+x[-2])
-    return len(x)-1
+        x.append(x[-1] + x[-2])
+    return len(x) - 1
+
 
 print("Введите число, до которого будут искаться числа Фибоначчи: ")
 n = int(input())
-fibonacci = lambda n: reduce(lambda x, n: x + [x[-1] + x[-2]], range(find_quantity(n) - 2), [0, 1])
+fibonacci_range = range(find_quantity(n) - 2)
+fibonacci = lambda n: reduce(lambda x, n:
+                             x + [x[-1] + x[-2]], fibonacci_range, [0, 1])
 print(*fibonacci(n), sep=', ')
