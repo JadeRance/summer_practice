12441e40b6edadc0d11ab4a6ea18b8056b85a194
Q100V <dvk97@tpu.ru>
2023-07-22 10:53:34 +0700

    Pt_2_8: Powered number greater than entered
    Enter the number to find powered number greater than entered.

--- Pt_2_8.py
+++ Pt_2_8.py
@@ -1,4 +1,5 @@
 import math
 print("Введите число:")
 value = int(input())
-print("Число, чей квадрат больше введеного значения:", math.ceil(math.sqrt(value)))
+result = math.ceil(math.sqrt(value))
+print("Число, чей квадрат больше введеного значения:", result)
