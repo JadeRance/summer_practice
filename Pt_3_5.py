709b6c324a13ae67a7fca87a4adb181a842aca5c
Q100V <dvk97@tpu.ru>
2023-07-22 11:19:45 +0700

    Pt_3_5: List of simple numbers
    Find all simple numbers up to the entered value.

--- Pt_3_5.py
+++ Pt_3_5.py
@@ -1,9 +1,9 @@
 import math
 n = int(input())
 simple = []
-for i in range(2, n+1):
+for i in range(2, n + 1):
     count = 0
-    for j in range(2, i+1):
+    for j in range(2, i + 1):
         if i % j == 0:
             count += 1
     if count == 1:
