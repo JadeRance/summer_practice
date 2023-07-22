91a151895019e4828dbe36cc5c2b965eee767d66
Q100V <dvk97@tpu.ru>
2023-07-22 11:38:06 +0700

    Pt_4_3: Simple values between two numbers
    Enter two numbers to find all simole values between them.

--- Pt_4_3.py
+++ Pt_4_3.py
@@ -2,9 +2,9 @@ import math
 low = int(input())
 high = int(input())
 simple_numbers = []
-for i in range(low, high+1):
+for i in range(low, high + 1):
     divider_count = 0
-    for j in range(1, math.floor(math.sqrt(i))+1):
+    for j in range(1, math.floor(math.sqrt(i)) + 1):
         if i % j == 0:
             divider_count += 1
     if divider_count == 1:
