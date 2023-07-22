b25cb1818e85e8b232227b2fb4024cdee6ddac17
Q100V <dvk97@tpu.ru>
2023-07-22 10:54:52 +0700

    Pt_2_9: Indeces of greatest digit
    Enter the number to find indeces of greatest digit from the beginning
    of the number and from the end.

--- Pt_2_9.py
+++ Pt_2_9.py
@@ -1,6 +1,7 @@
 print("Введите число: ")
 value = input()
 numbers = list(map(int, value))
-fwd_number = numbers.index(max(numbers))+1
+fwd_number = numbers.index(max(numbers)) + 1
 back_number = len(numbers) - numbers.index(max(numbers))
-print("Индекс самой большой цифры с начала: ", fwd_number, '\n', "Индекс самой большой цифры с конца: ", back_number, sep='')
+print("Индекс самой большой цифры с начала:", fwd_number)
+print("Индекс самой большой цифры с конца:", back_number)
