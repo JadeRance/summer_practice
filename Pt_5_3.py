44f9e2268a41ace6af334991008e46043d019094
Q100V <dvk97@tpu.ru>
2023-07-22 11:54:51 +0700

    Pt_5_3: Books in the specified interval
    Enter two years to find books written in that period.

--- Pt_5_3.py
+++ Pt_5_3.py
@@ -13,6 +13,8 @@ result = []
 for i in range(1, len(books_list)):
     if int(books_list[i][3]) >= low and int(books_list[i][3]) <= high:
         result.append(books_list[i][1])
-if not(result):
+if not (result):
     print("Нет книг в заданном диапазоне")
-else: print(*result, sep=', ')
+else:
+    print(*result, sep=', ')
+    
