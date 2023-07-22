d6125c96e6679f1d5f4c8d00cd39e0edf60c9603
Q100V <dvk97@tpu.ru>
2023-07-22 11:24:40 +0700

    Pt_3_9: Dictionary with powered numbers
    Make the dictionary with numbers from 1 to 10 with their powered
    values.

--- Pt_3_9.py
+++ Pt_3_9.py
@@ -1,3 +1,3 @@
-dict = {x: x+1 for x in range(10)}
-result = {dict[x]: dict[x]*dict[x] for x in range(len(dict))}
+dict = {x: x + 1 for x in range(10)}
+result = {dict[x]: dict[x] * dict[x] for x in range(len(dict))}
 print(result)
