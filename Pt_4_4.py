3acbf0fe8b8bf0d7d3bbe7324d2aa8cc2f6fd4be
Q100V <dvk97@tpu.ru>
2023-07-22 11:40:35 +0700

    Pt_4_4: Sum from numbers
    Enter the sum to find all possible summations from list with numbers.

--- Pt_4_4.py
+++ Pt_4_4.py
@@ -1,7 +1,10 @@
 from itertools import chain, combinations
 
+
 def powerset(numbers, s):
-    return chain.from_iterable(combinations(numbers, s) for s in range(len(numbers)+1))
+    return chain.from_iterable(combinations(numbers, s)
+                               for s in range(len(numbers) + 1))
+
 
 def cut_numbers(numbers):
     for i in range(len(numbers)):
@@ -9,12 +12,14 @@ def cut_numbers(numbers):
             del numbers[i:]
             break
 
+
 def find_s(numbers):
     total = 0
     for i in range(len(numbers)):
         if total + numbers[i] < summation:
             total += 1
     return total
+
 
 numbers = sorted([1, 2, 2, 4, 5, 7])
 summation = int(input())
