b7d220c5bd0cbd62d728a7e6514ea1b617021f52
Q100V <dvk97@tpu.ru>
2023-07-22 11:26:17 +0700

    Pt_3_12: Dictionary of powered even numbers
    Make the dictionary with even numbers and their powered values.

--- Pt_3_12.py
+++ Pt_3_12.py
@@ -1,3 +1,3 @@
-dict = {x: (x+1)*2 for x in range(5)}
-result = {dict[x]: dict[x]*dict[x]*dict[x] for x in range(len(dict))}
+dict = {x: (x + 1) * 2 for x in range(5)}
+result = {dict[x]: dict[x] * dict[x] * dict[x] for x in range(len(dict))}
 print(result)
